d7479366ba6080181f2a240ebf606dc4bafef07a
JonShort <JonShort@users.noreply.github.com>
2021-05-12 21:06:04 +0100

    1.0.0 release

--- Formula/cenv.rb
+++ Formula/cenv.rb
@@ -2,8 +2,8 @@ class Cenv < Formula
   desc "Environment variable file changer"
   homepage "https://github.com/JonShort/cenv"
   url "https://github.com/JonShort/cenv/releases/latest/download/cenv-macos-x86_64.tar.gz"
-  version "0.0.1"
-  sha256 "184a76c8c622aa15a601f9a0c806c071570c6d61bb403da2b382fcbf523b59c5"
+  version "1.0.0"
+  sha256 "5b98e1f814074dc31159d2c2abf3c0134b428af0da34963f9466a9ad391004c7"
   license "MIT"
   head "https://github.com/JonShort/cenv.git"
 
