31166a68b5d703ffba89e46921b015f7766563f8
JonShort <JonShort@users.noreply.github.com>
2021-08-02 19:58:09 +0100

    1.1.0 release

--- Formula/cenv.rb
+++ Formula/cenv.rb
@@ -2,8 +2,8 @@ class Cenv < Formula
   desc "Environment variable file changer"
   homepage "https://github.com/JonShort/cenv"
   url "https://github.com/JonShort/cenv/releases/latest/download/cenv-macos-x86_64.tar.gz"
-  version "1.0.0"
-  sha256 "5b98e1f814074dc31159d2c2abf3c0134b428af0da34963f9466a9ad391004c7"
+  version "1.1.0"
+  sha256 "ec14474d0f90d05ccf36111281497a82e6038ec44035a405a691370cc81a9f03"
   license "MIT"
   head "https://github.com/JonShort/cenv.git"
 
@@ -12,6 +12,6 @@ class Cenv < Formula
   end
 
   test do
-    assert_match "Problem parsing arguments: Keyword missing", `#{bin}/cenv`.chomp
+    assert_match "Problem reading .env file: Unable to read .env file", `#{bin}/cenv`.chomp
   end
 end
